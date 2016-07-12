94f56fc69e1780a82550a4be384324a6c89b2475
Emmanuel Albania <ej.albania@gmail.com>
2016-07-13 00:55:51 +0800

    no message

--- wp-content/themes/html5blank-stable/template-aboutus.php
+++ wp-content/themes/html5blank-stable/template-aboutus.php
@@ -19,40 +19,6 @@
 				<br class="clear">
 				<?php edit_post_link(); ?>
 
-				<?php 
-					$query = new WP_Query(array('post_type' => 'post','category_name' => 'staff'));
-					$posts = $query->get_posts();
-					echo "<pre>";
-					print_r($posts);
-					echo "</pre><br><br><br>";
-
-					// The Loop
-					if ( $query->have_posts() ) {
-						echo '<ul>';
-						while ( $query->have_posts() ) {
-							$query->the_post();
-							$image = rwmb_meta( 'your_prefix_image', 'type=image&size=meduim' );
-							// echo $image['url'];
-							// 
-							echo "<pre>";
-							print_r($image);
-							echo "</pre><br><br>". $image ."sdf<br>";
-
-							// echo "<a href='{" . $image['261']['full_url'] . "}' rel='lightbox'><img src='{" . $image['url'] . "}' width='{" . $image['width'] . "}' height='{" . $image['height'] . "}' alt='{" . $image['alt'] . "}' /></a>";
-							echo rwmb_meta( 'your_prefix_jobdesc', $args = array(), $post_id = get_the_id()) . ' of ' . rwmb_meta( 'your_prefix_company', $args = array(), $post_id = get_the_id());
-							echo '<li>' . get_the_title() . '</li>';
-							echo '<li>' . get_the_content() . '</li><br><br>';
-
-							
-						}
-						echo '</ul>';
-						/* Restore original Post Data */
-						wp_reset_postdata();
-					} else {
-						// no posts found
-					}
-				 ?>
-
 			</article>
 			<!-- /article -->
 
